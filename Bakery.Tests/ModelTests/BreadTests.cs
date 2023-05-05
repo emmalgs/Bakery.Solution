c55622e7107465e0975bb228668fa7416fd7fc4b
Emma Gerigscott <elgerig@gmail.com>
2023-05-05 08:45:59 -0700

    class Bread adds loafOrder when called + tests passing

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -6,6 +6,13 @@ namespace Bakery.Tests
   [TestClass]
   public class BreadTests
   {
-
+    [TestMethod]
+    public void GetLoafOrder_ReturnsLoadOrder_Int()
+    {
+      int loafOrder = 4;
+      Bread newBread = new Bread(loafOrder);
+      int result = newBread.Loaves;
+      Assert.AreEqual(loafOrder, result);
+    }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,10 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public int Loaves { get; set; }
+    public Bread(int loafOrder)
+    {
+      Loaves = loafOrder;
+    }
   }
 }
