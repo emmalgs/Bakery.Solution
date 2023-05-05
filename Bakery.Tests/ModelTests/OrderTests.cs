1c127910fe1cfdcda5498afd122fb743fcd6057e
Emma Gerigscott <elgerig@gmail.com>
2023-05-05 10:47:06 -0700

    add GetCost method and set cost field + tests passing

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,16 @@ namespace Bakery.Tests
       int result = newOrder.Amount;
       Assert.AreEqual(bakeryOrder, result);
     }
+
+    [TestMethod]
+    public void CostMethod_TakesPriceAndReturnsOrderCost_Int()
+    {
+      int bakeryOrder = 2;
+      int costPerItem = 3;
+      Order newOrder = new Order(bakeryOrder);
+      newOrder.GetCost(costPerItem);
+      int result = newOrder.Cost;
+      Assert.AreEqual(6, result);
+    }
   }
 }
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,9 +4,15 @@ namespace Bakery.Models
   public class Order
   {
     public int Amount { get; set; }
+    public int Cost { get; set; }
     public Order(int amount)
     {
       Amount = amount;
+    }
+    
+    public void GetCost(int price)
+    {
+      Cost = price * Amount;
     }
   }
 }
